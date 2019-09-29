068ca082ff2ad47ee2597ddd0fbea1e64536e3b5
Danniel Zelaya <dannielzelaya@gmail.com>
2019-09-28 19:29:32 -0500

    started over and created nav bar, added paralex, and content. Started Css

---
+++ assets/js/materialize.js
@@ -0,0 +1,11 @@
+// //function for carousel effect
+// document.addEventListener('DOMContentLoaded', function () {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, options);
+// });
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
