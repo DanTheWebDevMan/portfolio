2ca43bdf3f20ffdd8b20804534fe6fbb67ca3278
Danniel Zelaya <dannielzelaya@gmail.com>
2019-09-29 16:17:43 -0500

    chenge structure and tweak parallax

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -1,11 +1,10 @@
-// //function for carousel effect
-// document.addEventListener('DOMContentLoaded', function () {
-//     var elems = document.querySelectorAll('.carousel');
-//     var instances = M.Carousel.init(elems, options);
-// });
-
-// Or with jQuery
-
+// function for carousel effect
 $(document).ready(function () {
     $('.carousel').carousel();
 });
+
+//function for parallax effect:
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
