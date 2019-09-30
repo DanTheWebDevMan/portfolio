2b007896f5070b260181ff1b91a5737fc4a1ac6d
Danniel Zelaya <dannielzelaya@gmail.com>
2019-09-29 22:13:01 -0500

    added shadow effect

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -1,8 +1,15 @@
 // function for carousel effect
-$(document).ready(function () {
-    $(".dropdown-trigger").dropdown({ hover: false });
-    $('.carousel').carousel();
-    $('.parallax').parallax({ speed: 0.2 });
+// $(document).ready(function () {
+//     $(".dropdown-trigger").dropdown({ hover: false });
+// });
 
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+$(document).ready(function () {
+    $('.carousel').carousel();
 });
 
+$(document).ready(function () {
+    $('.parallax').parallax({ speed: 0.2 });
+});
