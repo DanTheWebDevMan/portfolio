aeceb79a7b56497f12ee1d111a83f3a07ea305a3
Danniel Zelaya <dannielzelaya@gmail.com>
2019-09-29 19:34:10 -0500

    fixed parallex by changin jquery src

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -1,10 +1,13 @@
 // function for carousel effect
 $(document).ready(function () {
     $('.carousel').carousel();
+    $('.parallax').parallax({ speed: 0.2 });
+    $(".dropdown-trigger").dropdown({ hover: false });
 });
 
-//function for parallax effect:
-document.addEventListener('DOMContentLoaded', function() {
+// dropdown funtion in nav-bar
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, options);
-  });
+    console.log(instances);
+});
